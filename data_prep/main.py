fb6ad3589eb33b534c5221f061e22dc4e4290fd6
Jerome Francis <francis.jerome999@gmail.com>
2025-09-28 13:20:47 -0400

    modify code to import data from domain1 data, variable name fixes

--- data_prep/main.py
+++ data_prep/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from make_prompt import make_prompt, make_documents
 from openai import OpenAI
+import json
+
 client = OpenAI(api_key="Open AI Key")
 
 
@@ -16,50 +18,54 @@ def makeTable():
 # Makes all 27 configurations for a given question
 def makeConfig(ques):
     configs = []
-    for dens in range(3):  # 3 distractor densities
-        for type in range(3):  # 3 types
-            for pos in range(3):  # 3 positions
-                configs.append((dens, type, pos))
+    for density in range(3):  # 3 distractor densityities
+        for inference_type in range(3):  # 3 types
+            for position in range(3):  # 3 positions
+                configs.append((density, inference_type, position))
     return configs
 
 # Fills the table with prompts and distractors
-def fillTable(numQues, tokens):
+def fillTable(tokens, question_data_path, output_path):
     export = makeTable()
     row_index = 1
+    question_data = []
+    with open(question_data_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            question_data.append(json.loads(line))
 
-    for i in range(numQues):
+    num_questions = len(question_data)
+    for i in range(num_questions):
         configs = makeConfig(i)  # Get all 27 configurations for this question
         
-        for config_idx, (dens, type, pos) in enumerate(configs):
+        for config_idx, (density, inference_type, position) in enumerate(configs):
             # Inserts name of the config into table
-            setup = ['Low', 'Medium', 'High'][dens] + '-' + ['N', 'T', 'P'][type] + '-' + ['Beg', 'Mid', 'End'][pos]
+            setup = ['Low', 'Medium', 'High'][density] + '-' + ['N', 'T', 'P'][inference_type] + '-' + ['Beg', 'Mid', 'End'][position]
             print(f"Question {i+1}, Config {config_idx+1}/27: {setup}")
             
-            if i < 4:
-                continue
-            if i == 4:
-                return
-                
-            export['Setup'][row_index] = setup
-
             # Generate documents and prompt
-            docs = make_documents(dens, type, pos, i, client)
-            prompt = make_prompt(dens, type, pos, i, docs, tokens, client)  # Using 40000 tokens as default
+            docs = make_documents(density, inference_type, position, i, client)
+            prompt = make_prompt(density, inference_type, position, i, docs, tokens, client)  # Using 40000 tokens as default
 
             # Inserts prompt, question, and correct answer into table
+            export['Setup'][row_index] = setup
             export['Prompt'][row_index] = prompt
-            export['Question'][row_index] = questions[i]
-            export['Gold Text'][row_index] = answers[i]
+            export['Question'][row_index] = question_data[i]['question']
+            export['Gold Text'][row_index] = question_data[i]['gold_text']
 
             # Inserts distractors into table
-            for j in range(len(docs)):
-                export['Distractor ' + str((j + 1)) + ' Text'][row_index] = docs[j]
-            
+            # for j in range(len(docs)):
+            #     export['Distractor ' + str((j + 1)) + ' Text'][row_index] = docs[j]
+            # print(question_data[i]['question'], question_data[i]['gold_text'])
+            # print("================================================")
             row_index += 1
+        
+        if i == 0:
+            break
 
     data = pd.DataFrame(export)
-    data.to_excel("Distractors.xlsx")
+    data.to_excel(output_path)
 
 # Calls the function
 if __name__ == "__main__":
-    fillTable(81, 8000)
+    fillTable(tokens=8000, question_data_path='qbank_by_domain/01_nq_closed_book.jsonl', output_path='domain1_data_final.xlsx')
