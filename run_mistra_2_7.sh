b2152fcc32115a9f2553cc9c24d987b18d5f161a
kj-ocuopo <sung32123@gmail.com>
2025-10-14 05:24:37 +0000

    add mistra output 5,6,7

--- run_mistra_2_7.sh
+++ run_mistra_2_7.sh
@@ -7,31 +7,31 @@
 # ./litm_repo_with_changes_v0.1/qa_data/domain6_data_06_policy_compliance_8000_converted.jsonl
 # ./litm_repo_with_changes_v0.1/qa_data/domain7_data_07_expert_legal_med_8000_converted.jsonl
 
-#1
-python ./mistral/mistral_process.py \
-    --input-path ./litm_repo_with_changes_v0.1/qa_data/domain1_data_nq_closed_book_8000_converted.jsonl \
-    --output-path ./mistral/output/domain1_data_nq_closed_book_8000_converted.mistral.jsonl \
-    --max-new-tokens 1000
+# #1
+# python ./mistral/mistral_process.py \
+#     --input-path ./litm_repo_with_changes_v0.1/qa_data/domain1_data_nq_closed_book_8000_converted.jsonl \
+#     --output-path ./mistral/output/domain1_data_nq_closed_book_8000_converted.mistral.jsonl \
+#     --max-new-tokens 1000
 
 
-#2
-python ./mistral/mistral_process.py \
-    --input-path ./litm_repo_with_changes_v0.1/qa_data/domain2_data_hotpot_citation_8000_converted.jsonl \
-    --output-path ./mistral/output/domain2_data_hotpot_citation_8000_converted.mistral.jsonl \
-    --max-new-tokens 1000
+# #2
+# python ./mistral/mistral_process.py \
+#     --input-path ./litm_repo_with_changes_v0.1/qa_data/domain2_data_hotpot_citation_8000_converted.jsonl \
+#     --output-path ./mistral/output/domain2_data_hotpot_citation_8000_converted.mistral.jsonl \
+#     --max-new-tokens 1000
 
 
-#3
-python ./mistral/mistral_process.py \
-    --input-path ./litm_repo_with_changes_v0.1/qa_data/domain3_data_novelhop_multihop_8000_converted.jsonl \
-    --output-path ./mistral/output/domain3_data_novelhop_multihop_8000_converted.mistral.jsonl \
-    --max-new-tokens 1000
+# #3
+# python ./mistral/mistral_process.py \
+#     --input-path ./litm_repo_with_changes_v0.1/qa_data/domain3_data_novelhop_multihop_8000_converted.jsonl \
+#     --output-path ./mistral/output/domain3_data_novelhop_multihop_8000_converted.mistral.jsonl \
+#     --max-new-tokens 1000
 
-#4
-python ./mistral/mistral_process.py \
-    --input-path ./litm_repo_with_changes_v0.1/qa_data/domain4_data_temporal_recency_8000_converted.jsonl \
-    --output-path ./mistral/output/domain4_data_temporal_recency_8000_converted.mistral.jsonl \
-    --max-new-tokens 1000
+# #4
+# python ./mistral/mistral_process.py \
+#     --input-path ./litm_repo_with_changes_v0.1/qa_data/domain4_data_temporal_recency_8000_converted.jsonl \
+#     --output-path ./mistral/output/domain4_data_temporal_recency_8000_converted.mistral.jsonl \
+#     --max-new-tokens 1000
 
 #5
 python ./mistral/mistral_process.py \
