4130a6220e2438a9a70028de9a6df74c2aaeeec6
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 <sung32123@gmail.com>
2025-10-14 05:28:06 +0000

    add run_script_2.sh

--- litm_repo_with_changes_v0.1/run_script_2.sh
+++ litm_repo_with_changes_v0.1/run_script_2.sh
@@ -1,12 +1,12 @@
- python -u ./scripts/get_qa_responses_from_longchat_ori.py \
-  --input-path qa_data/domain5_data_05_stackmath_numerical_8000_converted.jsonl \
-  --model lmsys/longchat-13b-16k \
-  --output-path qa_predictions/domain5_data_05_stackmath_numerical_8000_converted.output.jsonl.gz \
-  --num-gpus 2 \
-  --max-new-tokens 1000 \
-  --batch-size 4 \
-  --max-memory-per-gpu 50 \
-  --closedbook
+ # python -u ./scripts/get_qa_responses_from_longchat_ori.py \
+ #  --input-path qa_data/domain5_data_05_stackmath_numerical_8000_converted.jsonl \
+ #  --model lmsys/longchat-13b-16k \
+ #  --output-path qa_predictions/domain5_data_05_stackmath_numerical_8000_converted.output.jsonl.gz \
+ #  --num-gpus 2 \
+ #  --max-new-tokens 1000 \
+ #  --batch-size 4 \
+ #  --max-memory-per-gpu 50 \
+ #  --closedbook
 
 
 python -u ./scripts/get_qa_responses_from_longchat_ori.py \
