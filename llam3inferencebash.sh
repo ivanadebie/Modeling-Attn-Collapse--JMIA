ce35a21c90e8b9a241aae305adb3c04e2656d2c3
Jerome Francis <36845045+jerryfrancis-97@users.noreply.github.com>
2025-12-23 17:09:59 -0500

    Update llam3inferencebash.sh
    bash bugfix

--- llam3inferencebash.sh
+++ llam3inferencebash.sh
@@ -9,4 +9,4 @@ source atharv_env/bin/activate
 pip3 install -r requirements_atharv.txt
 echo "Installing libs"
 echo " run llama3 code"
-python3 llama3.py --input-path domain1_data_nq_closed_book_8000.jsonl --output_path domain1_llama3.1-8b_model_responses.jsonl --max-new-tokens 1000
+python3 llama3.py --input-path domain1_data_nq_closed_book_8000.jsonl --output-path domain1_llama3.1-8b_model_responses.jsonl --max-new-tokens 1000
