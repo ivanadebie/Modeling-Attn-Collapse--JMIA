51a8e865a8656944d95044f813fab0146b75e56c
Jerome Francis <36845045+jerryfrancis-97@users.noreply.github.com>
2025-12-23 17:08:54 -0500

    Update llam3inferencebash.sh
    remove colab based vresion

--- llam3inferencebash.sh
+++ llam3inferencebash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo " Starting code testing"
-!git clone -b Atharv https://github.com/ivanadebie/Modeling-Attn-Collapse--JMIA.git
-!cd Modeling-Attn-Collapse--JMIA
+git clone -b Atharv https://github.com/ivanadebie/Modeling-Attn-Collapse--JMIA.git
+cd Modeling-Attn-Collapse--JMIA
 echo "uploading git repo"
-!python3 -m venv atharv_env
-!source atharv_env/bin/activate
+python3 -m venv atharv_env
+source atharv_env/bin/activate
 pip3 install -r requirements_atharv.txt
 echo "Installing libs"
 echo " run llama3 code"
